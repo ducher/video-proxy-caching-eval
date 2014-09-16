675810d027f586210aabf3af8e03efee31a4bc20
Pierre Ducher <pierre.ducher@gmail.com>
2014-09-16 21:22:03 +0200

    Converted tab to spaces indentation. Much better.

--- simu.py
+++ simu.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 # helper functions for simulation purpose
 
 import time
@@ -6,58 +7,58 @@ import config
 
 
 def sleep(delay, transfer=True):
-	""" sleep function accelerated according to the speed of simulation """
+    """ sleep function accelerated according to the speed of simulation """
 
-	speed = config.speed
-	if not transfer:
-		speed = config.speed*config.wait_acc
-	time.sleep(delay/speed)
+    speed = config.speed
+    if not transfer:
+        speed = config.speed*config.wait_acc
+    time.sleep(delay/speed)
 
 def sleepsched(delay):
-	sleep(delay, False)
+    sleep(delay, False)
 
 def time_(transfer=True):
-	""" returns the time according to the simulation.
-			if transfer is true, it won't take the the additionnal acceleration
-			for the waiting periods into account
-	"""
-	speed = config.speed
-	if not transfer:
-		speed = config.speed*config.wait_acc
+    """ returns the time according to the simulation.
+            if transfer is true, it won't take the the additionnal acceleration
+            for the waiting periods into account
+    """
+    speed = config.speed
+    if not transfer:
+        speed = config.speed*config.wait_acc
 
-	return time.monotonic() * speed
+    return time.monotonic() * speed
 
 base_time = 0
 """ can be incremented to skip through time """
 
 def timesched():
-	global base_time
-	return time_(False) + base_time
+    global base_time
+    return time_(False) + base_time
 
 def add_time(amount):
-	""" skip through time """
-	global base_time
-	base_time += amount
+    """ skip through time """
+    global base_time
+    base_time += amount
 
 def real_time(time):
-	""" converts simulation time to real time """
-	return time * config.speed
+    """ converts simulation time to real time """
+    return time * config.speed
 
 # def no_active_download(clients):
-# 	""" returns true if no client from the list is currently downloading """
-# 	""" Inefficient way to check if there are still downloads pending """
-# 	for client in clients:
-# 		if client.media_downloading > 0:
-# 			#print("Guilty: "+client.name)
-# 			return False
-# 	return True
+#   """ returns true if no client from the list is currently downloading """
+#   """ Inefficient way to check if there are still downloads pending """
+#   for client in clients:
+#       if client.media_downloading > 0:
+#           #print("Guilty: "+client.name)
+#           return False
+#   return True
 
 
 """ The following is used to determine when the system is not downloading antyhing """
 
 def default_action_when_zero():
-	print("System inactive!")
-	pass
+    print("System inactive!")
+    pass
 
 nb_dl = 0
 """ number of curent downloads """
@@ -67,27 +68,27 @@ action_when_zero = default_action_when_zero
 """ Function to call when nb_dl == 0 """
 
 def inc_nb_dl():
-	""" Function to be called when a new download is pending """
-	global nb_dl
-	global lock
-	with lock:
-		nb_dl += 1
-	#print("State nb_dl: "+str(nb_dl))
+    """ Function to be called when a new download is pending """
+    global nb_dl
+    global lock
+    with lock:
+        nb_dl += 1
+    #print("State nb_dl: "+str(nb_dl))
 
 
 def dec_nb_dl():
-	""" Function to be called when a download is over """
-	global nb_dl
-	global lock
-	with lock:
-		nb_dl -= 1
-	if nb_dl < 1:
-		action_when_zero()
-	#print("State nb_dl: "+str(nb_dl))
+    """ Function to be called when a download is over """
+    global nb_dl
+    global lock
+    with lock:
+        nb_dl -= 1
+    if nb_dl < 1:
+        action_when_zero()
+    #print("State nb_dl: "+str(nb_dl))
 
 def no_active_download(clients=None):
-	""" Returns true if the number of downloads is zero.
-		Parameter is for backward compatibility only.
-	"""
-	global nb_dl
-	return nb_dl == 0
+    """ Returns true if the number of downloads is zero.
+        Parameter is for backward compatibility only.
+    """
+    global nb_dl
+    return nb_dl == 0
